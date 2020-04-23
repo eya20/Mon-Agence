33a36f71860463553e2f55fd42121c4a597367c3
Eya <bentali.aya@gmail.com>
2020-04-24 00:57:10 +0100

    doctrin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PropertyRepository;
 
 class HomeController extends AbstractController 
 {
   
 
-    public function index()
+    public function index(PropertyRepository $repository)
     {
-        return $this->render('pages/home.html.twig');
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig',['properties' => $properties]);
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,12 +5,45 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\PropertyRepository;
+use App\Entity\Property;
 
 class PropertyController extends AbstractController {
 
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository,EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
 
     public function index(): Response
     {
+        
         return $this->render('property/index.html.twig', ['current_menu'=> 'properties']);
+    }
+
+
+    public function show(Property $property,string $slug): Response
+    {
+        if ($property->getSlug() !== $slug){
+             return $this->redirectToRoute('property',
+             [
+                 'id' => $property->getId(),
+                 'slug' => $property->getSlug()
+             ], 301);
+        }
+        return $this->render('property/show.html.twig', ['current_menu'=> 'properties', 'property' => $property]);
     }
 }
